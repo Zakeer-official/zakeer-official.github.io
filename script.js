aa5f5ba4cd861c8d2acab4881355f40bda1c75fd
Zakeer Ahamad Shaik <102544273+Zakeer-official@users.noreply.github.com>
2024-08-27 01:20:00 +0530

    Update

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('menu-icon').addEventListener('click', () => {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+});
